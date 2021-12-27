d427c92bbe9b6ef0b0de7f1b8e1a47cb49a6f928
Daizc_ROG <daizc311@gmail.com>
2021-12-28 02:00:56 +0800

    暂时注释javaFX

--- src/main/java/club/dreamccc/ehtdownload/gui/PrimaryController.java
+++ src/main/java/club/dreamccc/ehtdownload/gui/PrimaryController.java
@@ -1,14 +1,14 @@
-package club.dreamccc.ehtdownload.gui;
-
-import club.dreamccc.ehtdownload.App;
-import javafx.fxml.FXML;
-
-import java.io.IOException;
-
-public class PrimaryController {
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
-}
+//package club.dreamccc.ehtdownload.gui;
+//
+//import club.dreamccc.ehtdownload.App;
+//import javafx.fxml.FXML;
+//
+//import java.io.IOException;
+//
+//public class PrimaryController {
+//
+//    @FXML
+//    private void switchToSecondary() throws IOException {
+//        App.setRoot("secondary");
+//    }
+//}
--- src/main/java/club/dreamccc/ehtdownload/gui/SecondaryController.java
+++ src/main/java/club/dreamccc/ehtdownload/gui/SecondaryController.java
@@ -1,14 +1,14 @@
-package club.dreamccc.ehtdownload.gui;
-
-import club.dreamccc.ehtdownload.App;
-import javafx.fxml.FXML;
-
-import java.io.IOException;
-
-public class SecondaryController {
-
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
-}
+//package club.dreamccc.ehtdownload.gui;
+//
+//import club.dreamccc.ehtdownload.App;
+//import javafx.fxml.FXML;
+//
+//import java.io.IOException;
+//
+//public class SecondaryController {
+//
+//    @FXML
+//    private void switchToPrimary() throws IOException {
+//        App.setRoot("primary");
+//    }
+//}
